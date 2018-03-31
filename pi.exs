f435cef0183bf1f174e7c93ff0e243fbfb7645e2
Alexei <iamalexei@outlook.com>
2018-03-31 11:24:45 +0300

    Make factor and sigma function private

--- pi.exs
+++ pi.exs
@@ -1,10 +1,10 @@
 defmodule Math do
-  def factor(0) do 1 end
-  def factor(1) do 1 end
-  def factor(n) when n > 1 do n * factor(n - 1) end
+  defp factor(0) do 1 end
+  defp factor(1) do 1 end
+  defp factor(n) when n > 1 do n * factor(n - 1) end
 
-  def sigma(0) do 13591409 / 1 end
-  def sigma(n) when n > 0 do
+  defp sigma(0) do 13591409 / 1 end
+  defp sigma(n) when n > 0 do
     sigma(n - 1) + (Math.factor(6 * n) * (545140134 * n + 13591409)) / 
     (Math.factor(3 * n) * :math.pow(Math.factor(n), 3) * 
     :math.pow(-262537412640768000, n))
