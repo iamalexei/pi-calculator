aceaeee42972320d6d75920cf3fedc20ed2326cc
Alexei <iamalexei@outlook.com>
2018-04-06 20:29:05 +0300

    Make the factor() function a tail call optimized

--- pi.exs
+++ pi.exs
@@ -1,16 +1,20 @@
 defmodule Math do
-  defp factor(0) do 1 end
-  defp factor(1) do 1 end
-  defp factor(n) when n > 1 do n * factor(n - 1) end
+  defp factor(n, accum \\ 1) do 
+    if (n < 2) do 
+      accum
+    else
+      factor(n - 1, n * accum)
+    end
+  end
 
   defp sigma(0) do 13591409 / 1 end
   defp sigma(n) when n > 0 do
-    sigma(n - 1) + (Math.factor(6 * n) * (545140134 * n + 13591409)) / 
-    (Math.factor(3 * n) * :math.pow(Math.factor(n), 3) * 
+    sigma(n - 1) + (factor(6 * n) * (545140134 * n + 13591409)) / 
+    (factor(3 * n) * :math.pow(factor(n), 3) * 
     :math.pow(-262537412640768000, n))
   end
 
   def pi do
-    (426880 * :math.sqrt(10005)) / Math.sigma(1)
+    (426880 * :math.sqrt(10005)) / sigma(10)
   end
 end
